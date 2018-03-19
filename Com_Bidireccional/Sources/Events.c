53e66fe567ec88ce541d020792d164e5061756eb
lujano97 <lugaluchinchilla@gmail.com>
2018-03-19 04:57:17 -0400

    Ultimo Firmware

---
+++ Com_Bidireccional/Sources/Events.c
@@ -0,0 +1,292 @@
+/* ###################################################################
+**     Filename    : Events.c
+**     Project     : Integracion_Fase1
+**     Processor   : MC9S08QE128CLK
+**     Component   : Events
+**     Version     : Driver 01.02
+**     Compiler    : CodeWarrior HCS08 C Compiler
+**     Date/Time   : 2018-02-03, 08:14, # CodeGen: 0
+**     Abstract    :
+**         This is user's event module.
+**         Put your event handler code here.
+**     Settings    :
+**     Contents    :
+**         No public methods
+**
+** ###################################################################*/
+/*!
+** @file Events.c
+** @version 01.02
+** @brief
+**         This is user's event module.
+**         Put your event handler code here.
+*/         
+/*!
+**  @addtogroup Events_module Events module documentation
+**  @{
+*/         
+/* MODULE Events */
+
+
+#include "Cpu.h"
+#include "Events.h"
+
+/* User includes (#include below this line is not maintained by Processor Expert) */
+
+void TI1_OnInterrupt(void)
+{
+  /* Write your code here ... */
+		
+	if (estado_trigger == TRIGGER_TERMINADO){
+	}
+	else if (estado_trigger == TRIGGER_BAJO){
+		Bit1_SetVal();
+		estado_trigger = TRIGGER_ALTO;
+		}
+	else if (estado_trigger == TRIGGER_ALTO){
+		Bit1_ClrVal();
+		estado_trigger = TRIGGER_TERMINADO;
+		}
+}
+
+
+
+/*
+** ===================================================================
+**     Event       :  AS2_OnError (module Events)
+**
+**     Component   :  AS2 [AsynchroSerial]
+**     Description :
+**         This event is called when a channel error (not the error
+**         returned by a given method) occurs. The errors can be read
+**         using <GetError> method.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+// Interrupcion Rx
+void  AS2_OnError(void)
+{
+}
+
+/*
+** ===================================================================
+**     Event       :  AS2_OnRxChar (module Events)
+**
+**     Component   :  AS2 [AsynchroSerial]
+**     Description :
+**         This event is called after a correct character is received.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled and either the <Receiver>
+**         property is enabled or the <SCI output mode> property (if
+**         supported) is set to Single-wire mode.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void  AS2_OnRxChar(void)
+{
+	 unsigned char rx_c;
+	 unsigned int n_canales = 0;
+	 unsigned char Trama_PC2[3]={0xf1, 0x00, 0x00};
+	 unsigned int Blq;
+	 unsigned char CodError;
+	 CodError =  AS1_RecvChar(&rx_c);
+	 if ((rx_c & 0xf0  ) == 0xf0) {
+		 n_canales = rx_c & 0x0f;
+		 CodError = AS1_RecvBlock(&Blq, n_canales, &n_canales);
+		 CodError = AS1_SendBlock(Trama_PC2,n_canales,&n_canales);
+	 }
+	
+}
+
+/*
+** ===================================================================
+**     Event       :  AS2_OnTxChar (module Events)
+**
+**     Component   :  AS2 [AsynchroSerial]
+**     Description :
+**         This event is called after a character is transmitted.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void  AS2_OnTxChar(void)  
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  AS2_OnFullRxBuf (module Events)
+**
+**     Component   :  AS2 [AsynchroSerial]
+**     Description :
+**         This event is called when the input buffer is full;
+**         i.e. after reception of the last character 
+**         that was successfully placed into input buffer.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void  AS2_OnFullRxBuf(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  AS2_OnFreeTxBuf (module Events)
+**
+**     Component   :  AS2 [AsynchroSerial]
+**     Description :
+**         This event is called after the last character in output
+**         buffer is transmitted.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void  AS2_OnFreeTxBuf(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  AS1_OnError (module Events)
+**
+**     Component   :  AS1 [AsynchroSerial]
+**     Description :
+**         This event is called when a channel error (not the error
+**         returned by a given method) occurs. The errors can be read
+**         using <GetError> method.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void  AS1_OnError(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  AS1_OnRxChar (module Events)
+**
+**     Component   :  AS1 [AsynchroSerial]
+**     Description :
+**         This event is called after a correct character is received.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled and either the <Receiver>
+**         property is enabled or the <SCI output mode> property (if
+**         supported) is set to Single-wire mode.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void  AS1_OnRxChar(void)
+{
+  Bit8_NegVal();
+}
+
+/*
+** ===================================================================
+**     Event       :  AS1_OnTxChar (module Events)
+**
+**     Component   :  AS1 [AsynchroSerial]
+**     Description :
+**         This event is called after a character is transmitted.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void  AS1_OnTxChar(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  AS1_OnFullRxBuf (module Events)
+**
+**     Component   :  AS1 [AsynchroSerial]
+**     Description :
+**         This event is called when the input buffer is full;
+**         i.e. after reception of the last character 
+**         that was successfully placed into input buffer.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void  AS1_OnFullRxBuf(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  AS1_OnFreeTxBuf (module Events)
+**
+**     Component   :  AS1 [AsynchroSerial]
+**     Description :
+**         This event is called after the last character in output
+**         buffer is transmitted.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void  AS1_OnFreeTxBuf(void)
+{
+  /* Write your code here ... */
+}
+
+/*
+** ===================================================================
+**     Event       :  Cap1_OnCapture (module Events)
+**
+**     Component   :  Cap1 [Capture]
+**     Description :
+**         This event is called on capturing of Timer/Counter actual
+**         value (only when the component is enabled - <Enable> and the
+**         events are enabled - <EnableEvent>.This event is available
+**         only if a <interrupt service/event> is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void Cap1_OnCapture(void)
+{
+	/* Write your code here ... */
+			
+		if (estado_echo == ECHO_TRIGGERED){
+			Bit7_SetVal();
+			Cap1_Reset(); // Se detecta el Rising edge, resetear registro de captura
+			estado_echo = ECHO_MEDIR;
+		}
+		else if (estado_echo == ECHO_MEDIR){
+			Cap1_GetCaptureValue(&medicion); // Se guarda el valor medido en el Falling edge
+			estado_echo= ECHO_TERMINADO;
+			}
+		else if (estado_echo == ECHO_TERMINADO){
+
+			}
+  /* Write your code here ... */
+}
+
+/* END Events */
+
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.3 [05.09]
+**     for the Freescale HCS08 series of microcontrollers.
+**
+** ###################################################################
+*/
